0734b393e4480a0c38cc377c5a7a5f1e02bdad79
slaastenkaa <slaastenkaa@gmail.com>
2019-10-05 00:50:18 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = "family",
+  knowsProgramming = true,
+  config = { family: 4 },
+  preferences = [focus]
+) {
+  let minLearn = 500;
+  let basic = 800;
+  let master = minLearn + basic;
+  let speedConfig = config[preferences];
+  let time = knowsProgramming ? basic : master;
+
+  return Math.ceil(time / speedConfig);
+};
